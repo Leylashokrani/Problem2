919746612f58cd76f969a506f05546df99770c73
Leylashokrani <lilish672@yahoo.com>
2022-06-21 12:15:12 +0430

    first

---
+++ Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explanation1
+{
+    internal class Class1
+    {
+
+
+        // function that will
+        // calculate the factorial
+        static long factorial(int N)
+        {
+            int result = 1;
+            while (N > 0)
+            {
+                result = result * N;
+                N--;
+            }
+            return result;
+        }
+
+        static long countWays(int N)
+        {
+            long total = factorial(N + N);
+            long total1 = factorial(N);
+            return (total / total1) / total1;
+        }
+
+        // Driver code
+        public static void Main(String[] args)
+        {
+            int N = 5;
+            Console.WriteLine("Ways = " + countWays(N));
+        }
+    }
+}
+
+
+
+//namespace Explanation1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] temp = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2 ,1,3};
+//            int max = temp[0];
+//            int indexLast = 1;
+//            int sum = 0;
+//            for (int i = 1; i < temp.Length; i++)
+//            {
+//                if (temp[i - 1] < temp[i])
+//                {
+//                    for (int j = indexLast; j < i; j++)
+//                    {
+//                        if (temp[i] > temp[j] && temp[indexLast] > temp[j])
+//                        {
+//                            if (temp[i] > temp[indexLast])
+//                            {
+//                                sum = sum + (temp[indexLast] - temp[j]);
+//                                temp[j] = temp[j] + (temp[indexLast] - temp[j]);
+//                            }
+//                            else
+//                            { 
+//                                sum = sum + (temp[i] - temp[j]);
+//                                temp[j] = temp[j] + (temp[i] - temp[j]);
+
+//                            }
+
+//                        }
+
+//                    }
+//                }
+//                    if (max <= temp[i])
+//                {
+
+//                    indexLast = i;
+//                    max = temp[i];
+//                }
+
+//            }
+
+//            Console.WriteLine("Hello World!",sum);
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Net;
+//class MainClass
+//{
+//    static void Main()
+//    {
+//        int output = 0;
+//        WebRequest request = WebRequest.Create("https://coderbyte.com/api/challenges/json/age-counting");
+//        WebResponse response = request.GetResponse();
+//        String body = String.Empty;
+//        using (var stream = response.GetResponseStream())
+//        {
+//            using (var reader = new StreamReader(stream))
+//            {
+//                body = reader.ReadToEnd();
+//                string temp = body.Split(':')[1];
+//                var temp1 = temp.Remove(temp.Length - 2).Split(',');
+
+//                for (int i = 0; i < temp1.Length; i++)
+//                {
+//                    var temp2 = temp1[i].Split('=');
+//                    if (temp2[0].Trim() == "age")
+//                    {
+//                        if (Convert.ToInt16(temp2[1]) >= 50)
+//                        {
+//                            output++;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//       encode("26wzuk4t70", output);
+//        response.Close();
+//    }
+//    static void encode(String s, int k)
+//    {
+
+//        // changed string
+//        String newS = "";
+
+//        // iterate for every characters
+//        for (int i = 0; i < s.Length; ++i)
+//        {
+//            // ASCII value
+//            int val = s[i];
+//            // store the duplicate
+//            int dup = k;
+
+//            // if k-th ahead character exceed 'z'
+//            if (val + k > 122)
+//            {
+//                k -= (122 - val);
+//                k = k % 26;
+
+//                newS += (char)(96 + k);
+//            }
+//            else
+//            {
+//                newS += (char)(96 + k);
+//            }
+
+//            k = dup;
+//        }
+
+//        // print the new string
+//        Console.Write(newS);
+//    }
+//}
+
+
+namespace Explanation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            int[] temp = new int[100];
+
+            Console.Write("\n\nRead n number of values in an array and display it in reverse order:\n");
+            Console.Write("------------------------------------------------------------------------\n");
+
+            Console.Write("Input the number of elements to store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Input {0} number of elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                temp[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+ 
+            int max = temp[0];
+            int indexLast = 1;
+            int sum = 0;
+            //number list
+            for ( i = 1; i < temp.Length; i++)
+            {
+                //
+                if (temp[i - 1] < temp[i])
+                {
+                    for (int j = indexLast; j < i; j++)
+                    {
+                        if (temp[i] > temp[j] && temp[indexLast] > temp[j])
+                        {
+                            if (temp[i] > temp[indexLast])
+                            {
+                                sum = sum + (temp[indexLast] - temp[j]);
+                                temp[j] = temp[j] + (temp[indexLast] - temp[j]);
+                            }
+                            else
+                            {
+                                sum = sum + (temp[i] - temp[j]);
+                                temp[j] = temp[j] + (temp[i] - temp[j]);
+
+                            }
+
+                        }
+
+                    }
+                }
+                if (max <= temp[i])
+                {
+
+                    indexLast = i;
+                    max = temp[i];
+                }
+
+            }
+
+            Console.WriteLine("Volume water:", sum);
+        }
+    }
+}
